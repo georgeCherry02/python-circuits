d57b6f4c905edcf6bf06753c52718445a1699cb3
GeorgeCherry02 <georgeb.cherry@gmail.com>
2024-06-19 17:23:30 +0100

    Enable transistor colouration

--- circuits/transistor.py
+++ circuits/transistor.py
@@ -24,9 +24,9 @@ class Transistor:
         return self.input.high() and self.signal.high()
 
     def draw(self, screen: Surface):
-        input_high = self.input.high()
-        signal_high = self.signal.high()
-        output_high = self.output.high()
+        input_colour = LIVE_COLOUR if self.input.high() else DEAD_COLOUR
+        signal_colour = LIVE_COLOUR if self.signal.high() else DEAD_COLOUR
+        output_colour = LIVE_COLOUR if self.output.high() else DEAD_COLOUR
         draw.circle(screen, DEAD_COLOUR, self.location, TRANSISTOR_SIZE, width=1)
         # Yes this is all dependent on transistor size
         # I'm not changing that yet...
@@ -34,11 +34,11 @@ class Transistor:
         ik_end = self.input.location + (5, 3)
         ok_start = self.input.location + (8, 3)
         ok_end = self.input.location + (11, 0)
-        draw.line(screen, DEAD_COLOUR, self.input.location, ik_start)
-        draw.line(screen, DEAD_COLOUR, ik_start, ik_end)
-        draw.line(screen, DEAD_COLOUR, ik_end, ok_start)
-        draw.line(screen, DEAD_COLOUR, ok_start, ok_end)
-        draw.line(screen, DEAD_COLOUR, ok_end, self.output.location)
+        draw.line(screen, input_colour, self.input.location, ik_start)
+        draw.line(screen, input_colour, ik_start, ik_end)
+        draw.line(screen, input_colour, ik_end, ok_start)
+        draw.line(screen, output_colour, ok_start, ok_end)
+        draw.line(screen, output_colour, ok_end, self.output.location)
         sig_mid = self.location + (0, 3)
-        draw.line(screen, DEAD_COLOUR, sig_mid, self.signal.location)
+        draw.line(screen, signal_colour, sig_mid, self.signal.location)
 
